89b477060515a5e66630a10a22660c25626637d2
Andreas Spannagel <as@onefid.com>
2021-07-22 16:29:34 +0200

    Update new path instructions for pyenv and poetry.

--- shell/python.zsh
+++ shell/python.zsh
@@ -21,11 +21,15 @@
 if [[ -o login ]]; then
   # Initialize pyenv
   if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init -)"
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
   fi
 
   # Initialize poetry
-  export PATH="$HOME/.poetry/bin:$PATH"
+  export PATH="$HOME//.local/bin:$PATH"
 
   # pip should only run if there is a virtualenv currently activated
   export PIP_REQUIRE_VIRTUALENV=true
