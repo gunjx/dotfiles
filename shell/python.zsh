c3ab409706edd242d611f8fd91577c55533723fd
Andreas Spannagel <andreas@perfect-id.com>
2021-09-17 12:04:17 +0200

    Fix poetry path.

--- shell/python.zsh
+++ shell/python.zsh
@@ -30,7 +30,7 @@ if [[ -o login ]]; then
 
   # Initialize poetry
   export PATH="$HOME/.local/bin:$PATH"
-  export PATH="$HOME/.poetry/env:$PATH"
+  export PATH="$HOME/.poetry/bin:$PATH"
 
   # pip should only run if there is a virtualenv currently activated
   export PIP_REQUIRE_VIRTUALENV=true
